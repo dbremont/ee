2167bf0f441a9d7c19121fb3d84a74b6493fe8cb
dvictoriano <darlynvictoriano@gmail.com>
2025-01-30 14:53:35 -0400

    remove

--- hello/obj_dir/hello_tb.d
+++
@@ -1,5 +0,0 @@
-hello_tb.o: ../hello_tb.cpp Vhello.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated.h
